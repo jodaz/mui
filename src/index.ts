979d55e06c4793b84c8ceb9034d997c10bc08e98
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-01-16 11:20:07 -0400

    Export default for all modules

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './Chart';
-export * from './CardProgress';
-export * from './LoadingButton';
+export { default as Chart } from './Chart';
+export { default as CardProgress } from './CardProgress';
+export { default as LoadingButton } from './LoadingButton';
